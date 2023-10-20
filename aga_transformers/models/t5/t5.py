feca384c213e4b6aee41bd7e99e4b5bef8f765d0
gigant <giganttheo@gmail.com>
2023-10-20 15:16:42 +0200

    change the t5 loading

--- aga_transformers/models/t5/t5.py
+++ aga_transformers/models/t5/t5.py
@@ -25,8 +25,7 @@ def load_t5(repo_path="t5-base", dtype=jnp.dtype("float32"), attention_kwargs=No
     if dtype == jnp.dtype("bfloat16"):
         model.params = model.to_bf16(model.params)
     model.params = adapt_relative_pos_bias(model.params)
-    params_with_graph = add_graph_to_params(model.params, graph)
-    return tokenizer, model, params_with_graph
+    return tokenizer, model, graph
 
 def preprocess_function(examples, tokenizer, max_length=512, prefix="summarize: ", text_column="transcript", padding='longest'):
     inputs = examples[text_column]
