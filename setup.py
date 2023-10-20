e434cd8a90a7e0605b0c951ab3bee650b450d150
gigant <giganttheo@gmail.com>
2023-10-20 16:50:11 +0200

    factorizing code in a library

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+	from setuptools import setup
+except BaseException:
+	from distutils.core import setup
+
+
+setup(name='aga_transformers',
+      version="0.0.1", #__version__,
+      description='Arbitrary Graph Attention Transformers',
+      author='Théo Gigant',
+      author_email='theo.gigant@l2s.centralesupelec.fr',
+      url='https://github.com/giganttheo/aga_transformers',
+      packages=['attnptrns'],
+      install_requires=[
+		'numpy',
+	],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.9',
+    ],
+     )
