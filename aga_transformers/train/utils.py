3aaa21ea8364ceaa605b04b0444fcec0f401108e
gigant <giganttheo@gmail.com>
2023-10-22 12:45:05 +0200

    replacing jnp with np bc of omnistaging related issues

--- aga_transformers/train/utils.py
+++ aga_transformers/train/utils.py
@@ -7,5 +7,5 @@ def get_apply_fn(model):
     this function can be used in flax.training.train_state.TrainState as an apply_fn instead of model.__call__
     otherwise the optimizer will not accept the graph with a different dtype as the params (even if we ignore the graph in the optimizer)
     """
-    apply_fn_with_graph = lambda params, graph, **kwargs: model.__call__(params=add_graph_to_params(params, graph), **kwargs)
+    apply_fn_with_graph = lambda params, graph, **kwargs: model.__call__(params=params, **kwargs)#(params=add_graph_to_params(params, graph), **kwargs)
     return apply_fn_with_graph
