b1232b65d2779074948f7e51f16bc2f5428110d8
gigant <giganttheo@gmail.com>
2023-11-27 20:04:40 +0100

    tie the relative position bias instead of copying it

--- aga_transformers/models/utils.py
+++ aga_transformers/models/utils.py
@@ -60,8 +60,8 @@ def tie_relative_pos_bias(module_class, repo_path):
   """
   module_class = module_class
   n_blocks = AutoConfig.from_pretrained(repo_path).num_layers
-  first_block_relative_attention_bias = {k: (k,'block','0','layer','0','SelfAttention','relative_attention_bias', 'embedding') for k in ['encoder', 'decoder']}
-  other_blocks_relative_attention_bias = {k: [(k,'block', str(b),'layer','0','SelfAttention','relative_attention_bias', 'embedding') for b in range(1, n_blocks)] for k in ['encoder', 'decoder']}
+  first_block_relative_attention_bias = {k: ('params', k,'block','0','layer','0','SelfAttention','relative_attention_bias', 'embedding') for k in ['encoder', 'decoder']}
+  other_blocks_relative_attention_bias = {k: [('params', k,'block', str(b),'layer','0','SelfAttention','relative_attention_bias', 'embedding') for b in range(1, n_blocks)] for k in ['encoder', 'decoder']}
   rules = {source:
           target for k, source in first_block_relative_attention_bias.items() for target in other_blocks_relative_attention_bias[k]}
   module_class = tie(module_class, rules, transpose=False)
