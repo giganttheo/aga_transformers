863f8b31c89b405537c7954bebe15190b680d697
gigant <giganttheo@gmail.com>
2023-11-28 17:43:47 +0100

    fix the batched version of t5 graph attn

--- aga_transformers/models/t5/modeling_t5.py
+++ aga_transformers/models/t5/modeling_t5.py
@@ -471,11 +471,9 @@ class FlaxT5Attention(nn.Module):
             print(f"1: {graph_mask.shape}")
             if attention_mask is not None:
                 # merge the input attention mask with the graph mask
-                # attn_mask_2_graph_mask = jax.vmap(jax.vmap(lambda mask, ids: mask[ids], in_axes=(None, 0)))
-                # graph_mask = graph_mask * attn_mask_2_graph_mask(attention_mask, receivers)
-                print(f"r: {receivers.shape}")
-                print(f"am: {attention_mask.shape}")
-                graph_mask = graph_mask * attention_mask[..., receivers]
+                attn_mask_2_graph_mask = jax.vmap(lambda mask, ids: mask[..., ids], in_axes=(None, 0), out_axes=(0))
+                graph_mask = graph_mask * attn_mask_2_graph_mask(attention_mask, receivers)
+                # graph_mask = graph_mask * attention_mask[..., receivers]
 
             # for fast decoding causal attention mask should be shifted
             causal_attention_mask_shift = (
