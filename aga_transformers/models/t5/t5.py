eb9d19fab5a10c4e6a94cbcb0020b8630ac7c964
gigant <giganttheo@gmail.com>
2023-12-04 18:46:14 +0100

    Resolved merge conflict 	modified:   aga_transformers/attention_patterns/utils.py 	modified:   aga_transformers/models/t5/t5.py 	modified:   aga_transformers/tests/t5.py

--- aga_transformers/models/t5/t5.py
+++ aga_transformers/models/t5/t5.py
@@ -3,19 +3,25 @@ import jax.numpy as jnp
 
 from .modeling_t5 import FlaxT5ForConditionalGeneration
 from ..utils import repeat_relative_pos_bias, add_graph_to_params, tie_graph_layers, tie_relative_pos_bias
+from ..utils import repeat_relative_pos_bias, add_graph_to_params, tie_graph_layers, tie_relative_pos_bias
 from ...attention_patterns.vanilla_attention.vanilla import create_dense_attn_patterns
 from ...attention_patterns.sparse_attention.led import create_led_attn_patterns
 
 #wrapper to load the model and preprocess the weights
 
 def load_t5(repo_path="t5-base", dtype="bfloat16", attention_mode="led", attention_kwargs=None, layer_wise=False, **model_kwargs):
+def load_t5(repo_path="t5-base", dtype="bfloat16", attention_mode="led", attention_kwargs=None, layer_wise=False, **model_kwargs):
     tokenizer = AutoTokenizer.from_pretrained(repo_path)
+    module_class = FlaxT5ForConditionalGeneration.module_class
+    module_class = tie_relative_pos_bias(module_class, repo_path)
+    FlaxT5ForConditionalGeneration.module_class = module_class
     module_class = FlaxT5ForConditionalGeneration.module_class
     module_class = tie_relative_pos_bias(module_class, repo_path)
     FlaxT5ForConditionalGeneration.module_class = module_class
     model = FlaxT5ForConditionalGeneration.from_pretrained(
         repo_path,
         **model_kwargs,
+        dtype=dtype,
         dtype=dtype,
     )
     if dtype == "bfloat16" or dtype == jnp.dtype("bfloat16"):
