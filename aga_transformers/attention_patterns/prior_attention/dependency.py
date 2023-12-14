6410b81792707dfca4c66d08a8483a97b65f477d
gigant <giganttheo@gmail.com>
2023-12-14 16:31:56 +0100

    update normalize fn for dependency graph construction

--- aga_transformers/attention_patterns/prior_attention/dependency.py
+++ aga_transformers/attention_patterns/prior_attention/dependency.py
@@ -13,6 +13,9 @@ nlp.add_pipe('benepar', config={'model': 'benepar_en3'})
 def dependency_parser(sentences):
   return [nlp(sentence) for sentence in sentences]
 
+def normalize(string):
+  return string.lower().replace("▁", "").replace(" ", "")
+
 class DependencyAttentionPattern(AttentionPattern):
   #Attention pattern constructed from the dependency graph, using the Berkeley Neural Parser model
   # https://github.com/nikitakit/self-attentive-parser
@@ -39,9 +42,7 @@ class DependencyAttentionPattern(AttentionPattern):
       return {"nodes": nodes, "edges": edges, "senders": senders, "receivers": receivers, "edge_labels": edge_labels}
 
     graph = construct_dependency_graph(dependency_parser(text))
-    # print(graph)
-    new_token_ids = get_new_token_ids(graph["nodes"], tokens)
-    # print(new_token_ids)
+    new_token_ids = get_new_token_ids(graph["nodes"], tokens, normalize_fn=normalize)
     new_edges = [(new_id_s, new_id_r) for (id_s, id_r) in graph["edges"] for new_id_r in new_token_ids[id_r] for new_id_s in new_token_ids[id_s]]
 
     receivers = np.array([edge[1] for edge in new_edges], dtype=np.uint16)
--- aga_transformers/attention_patterns/utils.py
+++ aga_transformers/attention_patterns/utils.py
@@ -33,7 +33,10 @@ def graph_from_path(tree, enc_self_attn, dec_self_attn, encdec_attn, path=[], la
       return None
   return {k: graph_from_path(t, enc_self_attn=enc_self_attn, dec_self_attn=dec_self_attn, encdec_attn=encdec_attn, path=path+[k]) for (k, t) in tree.items()}
 
-def map_segmentation_to_new_tokenizer(tokenized_1, tokenized_2, segments_1, tokenizer=None):
+def normalize(string):
+  return unidecode(string.lower().replace("▁", "").replace(" ", "")).casefold()
+
+def map_segmentation_to_new_tokenizer(tokenized_1, tokenized_2, segments_1, normalize_fn=normalize):
     # maps a segmentation that goes with a tokenized text, using a tokenization strategy (1)
     # to the relevant segmentation using another tokenization strategy (2)
     # # Example Usage
@@ -46,28 +49,26 @@ def map_segmentation_to_new_tokenizer(tokenized_1, tokenized_2, segments_1, toke
     segments_2 = []
     index_2 = 0
 
-    def normalize(string):
-      return unidecode(string.lower().replace("▁", "").replace(" ", "")).casefold()
     residual = ""
     for token_1, segment_1 in zip(tokenized_1, segments_1):
       if index_2 < len(tokenized_2):
-        tmp = residual + normalize(tokenized_2[index_2])
+        tmp = residual + normalize_fn(tokenized_2[index_2])
         num_tokens = 1
         index_2 += 1
-        if normalize(token_1) != '':
-          while index_2 < len(tokenized_2) and not (normalize(token_1) in tmp):
-            tmp += normalize(tokenized_2[index_2])
+        if normalize_fn(token_1) != '':
+          while index_2 < len(tokenized_2) and not (normalize_fn(token_1) in tmp):
+            tmp += normalize_fn(tokenized_2[index_2])
             index_2 += 1
             num_tokens += 1
-          residual = re.split(re.escape(normalize(token_1)), tmp, 1)[-1]
+          residual = re.split(re.escape(normalize_fn(token_1)), tmp, 1)[-1]
           segments_2.extend([segment_1]*num_tokens)
         else:
           segments_2.append(segment_1)
     return segments_2
 
-def get_new_token_ids(tokenized_1, tokenized_2):
+def get_new_token_ids(tokenized_1, tokenized_2, normalize_fn=normalize):
   # mapping[id] returns the list of tokens of tokenized_2 that correspond to tokenized_1[id]
-  segments_2 = map_segmentation_to_new_tokenizer(tokenized_1, tokenized_2, range(len(tokenized_1)))
+  segments_2 = map_segmentation_to_new_tokenizer(tokenized_1, tokenized_2, range(len(tokenized_1)), normalize_fn=normalize_fn)
   mapping = [[] for _ in tokenized_1]
   for i, segment in enumerate(segments_2):
     mapping[segment].append(i)
