ce26cc375fd9b3b1deff2e67ef3e4b46e8063e3d
gigant <giganttheo@gmail.com>
2023-12-21 17:02:28 +0100

    train bash script

--- train_globallocal.sh
+++ train_globallocal.sh
@@ -3,6 +3,7 @@ export https_proxy=http://webproxy.lab-ia.fr:8080
 export HTTP_PROXY=http://webproxy.lab-ia.fr:8080
 export HTTPS_PROXY=http://webproxy.lab-ia.fr:8080
 ls -ld /usr/local/cuda*
+export PATH=/usr/local/cuda-11.2/bin:$PATH.
 python ./train_lora.py \
 	--output_dir "./lora-t5-graph-base-16k" \
 	--model_name_or_path "google/flan-t5-base" \
