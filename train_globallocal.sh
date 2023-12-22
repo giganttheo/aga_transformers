fa2f5ad6ebbfdf23aa9a65ea8e5f28dfae222cf5
gigant <giganttheo@gmail.com>
2023-12-22 18:55:53 +0100

    train bash script

--- train_globallocal.sh
+++ train_globallocal.sh
@@ -3,8 +3,8 @@ export https_proxy=http://webproxy.lab-ia.fr:8080
 export HTTP_PROXY=http://webproxy.lab-ia.fr:8080
 export HTTPS_PROXY=http://webproxy.lab-ia.fr:8080
 
-# conda install cuda=11.2.0 cudnn cudatoolkit-dev -c nvidia
-# pip install  --proxy=http://webproxy.lab-ia.fr:8080 --upgrade "jax[cuda11_pip]"==0.4.20 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+conda install cuda=11.2.0 cudnn cudatoolkit-dev -c nvidia
+pip install  --proxy=http://webproxy.lab-ia.fr:8080 --upgrade "jax[cuda11_pip]"==0.4.20 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 export PATH=/usr/local/cuda-11.2/bin:$PATH.
 
@@ -20,9 +20,9 @@ python ./train_lora.py \
 	--predict_with_generate \
 	--num_train_epochs 3 \
 	--learning_rate 1e-6 \
-	--warmup_steps 10 \
-	--per_device_train_batch_size 1 \
-	--per_device_eval_batch_size 1 \
+	--warmup_steps 100 \
+	--per_device_train_batch_size 2 \
+	--per_device_eval_batch_size 2 \
 	--overwrite_output_dir \
 	--dtype "bfloat16" \
 	--max_target_length 512 \
