19f1177e2f7b47994dba9b4f819be7de4c99a819
gigant <giganttheo@gmail.com>
2023-12-29 13:13:40 +0100

    downgrade lorax

--- aga_transformers/train/lora.py
+++ aga_transformers/train/lora.py
@@ -32,13 +32,7 @@ def create_lora(model, optimizer, dtype="bfloat16"):
 
     # Split the parameters up into tunable and frozen ones, and initialize a pair of LoRA matrices for each parameter
     # which had a spec value other than LORA_FULL or LORA_FREEZE
-    lora_params = lorax.init_lora(model.params, lora_spec, jax.random.PRNGKey(0), dtype=dtype)
-
-    # `wrap_optimizer` uses the spec to freeze the appropriate subset
-    # of parameters.
-    # The frozen parameters won't have optimizer states etc
-    # created for them
-    optimizer = lorax.wrap_optimizer(optimizer, lora_spec)
+    frozen_params, lora_params = lorax.init_lora(model.params, lora_spec, jax.random.PRNGKey(0), dtype=dtype)
 
     # lorax.lora wraps a callable so that the arguments can be lorax.LoraWeight
     # instances. (It's actually just an alias for qax.use_implicit_args, so
@@ -46,4 +40,42 @@ def create_lora(model, optimizer, dtype="bfloat16"):
     lora_model = lorax.lora(model)
     apply_fn = lora_model.__call__
 
-    return apply_fn, lora_params, optimizer
+    return apply_fn, frozen_params, lora_params, optimizer
+
+# def create_lora(model, optimizer, dtype="bfloat16"):
+
+#     # This function defines a spec which tells lorax how each parameter should be handled
+#     def decision_fn(path, param):
+#         if 'embedding' in path:
+#             print(f'Fully finetuning param {path}')
+#             return LORA_FULL
+#         dim = 16
+#         # print(f'Using LoRA with dim={dim} for param {path}')
+#         return dim
+
+#     # Create a pytree with the same shape as params indicating how each parameter should be handled
+#     # Each leaf will be given one of the following values:
+#     # - LORA_FULL: The parameter will be fully finetuned
+#     # - LORA_FREEZE: The parameter will be frozen
+#     # - k > 0: The parameter will be LoRA tuned with a rank k update
+
+#     # Simple_spec is a helper to do this, but you can also create the label pytree yourself
+#     lora_spec = lorax.simple_spec(model.params, decision_fn=decision_fn, tune_vectors=True)
+
+#     # Split the parameters up into tunable and frozen ones, and initialize a pair of LoRA matrices for each parameter
+#     # which had a spec value other than LORA_FULL or LORA_FREEZE
+#     lora_params = lorax.init_lora(model.params, lora_spec, jax.random.PRNGKey(0), dtype=dtype)
+
+#     # `wrap_optimizer` uses the spec to freeze the appropriate subset
+#     # of parameters.
+#     # The frozen parameters won't have optimizer states etc
+#     # created for them
+#     optimizer = lorax.wrap_optimizer(optimizer, lora_spec)
+
+#     # lorax.lora wraps a callable so that the arguments can be lorax.LoraWeight
+#     # instances. (It's actually just an alias for qax.use_implicit_args, so
+#     # the wrapped function can handle other qax types as well)
+#     lora_model = lorax.lora(model)
+#     apply_fn = lora_model.__call__
+
+#     return apply_fn, lora_params, optimizer
--- aga_transformers/train/loss.py
+++ aga_transformers/train/loss.py
@@ -33,3 +33,30 @@ def loss_fn(
     loss = token_losses.sum() / decoder_attention_mask[:, 1:].sum()
     
     return loss, {'loss': loss}
+
+def lora_loss_fn(
+    model,
+    tunable_params: PyTree,
+    frozen_params: PyTree,
+    graph: PyTree,
+    input_ids: jax.Array, 
+    attention_mask: jax.Array, 
+    decoder_input_ids: jax.Array, 
+    decoder_attention_mask: jax.Array,
+    **model_kwargs
+) -> Tuple[jax.Array, PyTree]:
+    
+    model_output = model(
+        params={"params": (frozen_params, tunable_params), "graph": graph},
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        decoder_input_ids=decoder_input_ids, 
+        decoder_attention_mask=decoder_attention_mask,
+        **model_kwargs,
+    )
+    
+    target_logits = model_output.logits[:, :-1, :].astype(jnp.float32)
+    token_losses = optax.softmax_cross_entropy_with_integer_labels(target_logits, decoder_input_ids[:, 1:]) * decoder_attention_mask[:, 1:]
+    loss = token_losses.sum() / decoder_attention_mask[:, 1:].sum()
+    
+    return loss, {'loss': loss}
