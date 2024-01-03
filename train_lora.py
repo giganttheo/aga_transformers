0e70c2674e11aef36c86e48ac4cac5027e5a53e7
gigant <giganttheo@gmail.com>
2024-01-04 00:24:18 +0100

    optimize flags

--- train_lora.py
+++ train_lora.py
@@ -65,6 +65,23 @@ from aga_transformers.models.t5.t5 import load_t5
 from aga_transformers.train.lora import create_lora
 from aga_transformers.train.loss import loss_fn
 
+#XLA performance flags recommended by https://jax.readthedocs.io/en/latest/gpu_performance_tips.html#xla-performance-flags
+
+os.environ['XLA_FLAGS'] = (
+    '--xla_gpu_enable_triton_softmax_fusion=true '
+    '--xla_gpu_triton_gemm_any=True '
+    '--xla_gpu_enable_async_collectives=true '
+    '--xla_gpu_enable_latency_hiding_scheduler=true '
+    '--xla_gpu_enable_highest_priority_async_stream=true '
+)
+
+#NCCL flags recommended by https://jax.readthedocs.io/en/latest/gpu_performance_tips.html#nccl-flags
+
+os.environ.update({
+  "NCCL_LL128_BUFFSIZE": "-2",
+  "NCCL_LL_BUFFSIZE": "-2",
+   "NCCL_PROTO": "SIMPLE,LL,LL128",
+ })
 
 TF_CPP_MIN_LOG_LEVEL=0 
 print(f"Devices: {jax.devices()}")
