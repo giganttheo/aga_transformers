c5ce0b62d8d6d693e902cf7133cf67fe742293a1
gigant <giganttheo@gmail.com>
2024-01-17 17:11:36 +0100

    efficient block global local attention for t5 (inspired by longt5 implementation)

--- aga_transformers/models/t5/modeling_t5_efficient.py
+++ aga_transformers/models/t5/modeling_t5_efficient.py
@@ -722,6 +722,8 @@ class FlaxT5Attention(nn.Module):
             jax.debug.print("attn_weights:{attn_weights.shape}", attn_weights=attn_weights)
             jax.debug.print("value_states_blocks:{value_states_blocks.shape}", value_states_blocks=value_states_blocks)
             attn_output_blocks = jnp.einsum("... h q k , ... k h d ->... q h d", attn_weights, value_states_blocks)
+            shape_output = tuple(attn_output_blocks.shape[0], (attn_output_blocks.shape[1] * attn_output_blocks.shape[2])) + attn_output_blocks.shape[2:]
+            attn_output_blocks = attn_output_blocks.reshape(shape_output)[:, :seq_length, ...]
             # attn_output_blocks = einops.rearrange(attn_output_blocks, "... b q h d ->... (b q) h d") #unblock
 
             global_attn_weights = dot_product_attention_weights(
@@ -735,13 +737,13 @@ class FlaxT5Attention(nn.Module):
                 dtype=self.dtype,
             )
             attn_output_global = jnp.einsum("...hqk,...khd->...qhd", global_attn_weights, value_states)
-            
+
             # bring back to (batch_size, seq_length, d_model)
             jax.debug.print("global shape: {attn_output_global.shape}, local shape: {attn_output_blocks.shape}", attn_output_global=attn_output_global, attn_output_blocks=attn_output_blocks)
             # attn_output = jnp.concatenate([attn_output_global, attn_output_blocks], axis=1)
             
             attn_output_blocks = self._merge_heads(attn_output_blocks)
-            attn_output = attn_output_blocks[:, :seq_length, ...]
+            attn_output = attn_output_blocks#[:, :seq_length, ...]
             # jax.debug.print("output shape: {attn_output.shape}", attn_output=attn_output)
 
 
