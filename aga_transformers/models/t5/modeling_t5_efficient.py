3f9aadf1a3df21ade00548cc01156139826f43e8
gigant <giganttheo@gmail.com>
2024-01-17 13:05:39 +0100

    efficient block global local attention for t5 (inspired by longt5 implementation)

--- aga_transformers/models/t5/modeling_t5_efficient.py
+++ aga_transformers/models/t5/modeling_t5_efficient.py
@@ -613,7 +613,7 @@ class FlaxT5Attention(nn.Module):
         Self-attention (if key_value_states is None) or attention over source sentence (provided by key_value_states).
         """
         block_len= 16 #254+1  #TODO: add in config (radius + 1)
-        n_global_tokens = 16 #TODO: add in config
+        n_global_tokens = 512 #TODO: add in config
         
         batch_size, seq_length = hidden_states.shape[:2]
 
