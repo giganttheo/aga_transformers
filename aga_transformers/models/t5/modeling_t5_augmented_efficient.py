4a172f801dcf6c7258f2e9b49b75505e0a934cb4
gigant <giganttheo@gmail.com>
2024-02-03 23:26:13 +0100

    precompute edge biases

--- aga_transformers/models/t5/modeling_t5_augmented_efficient.py
+++ aga_transformers/models/t5/modeling_t5_augmented_efficient.py
@@ -1122,13 +1122,15 @@ class FlaxT5EfficientBlockGraphSelfAttention(nn.Module):
                 # edge_bias_local = self.graph_edge_bias(edge_bias_local[:, :1].swapaxes(1, -1)[..., 0]).swapaxes(1, -1)
                 if not precomputed:
                     edge_bias_local = edge_bias_local[:, 0]
-                edge_bias_local = jnp.where(edge_bias_local[..., None]>=0, self.graph_edge_bias(edge_bias_local), jnp.zeros(tuple(edge_bias_local.shape) + (1,), dtype=self.dtype))
+                # edge_bias_local = jnp.where(edge_bias_local[..., None]>=0, self.graph_edge_bias(edge_bias_local), jnp.zeros(tuple(edge_bias_local.shape) + (1,), dtype=self.dtype))
+                edge_bias_local = jax.lax.select(edge_bias_local[..., None]>=0, self.graph_edge_bias(edge_bias_local), jnp.zeros(tuple(edge_bias_local.shape) + (1,)).astype(self.dtype))
                 position_bias_local = position_bias_local + edge_bias_local.transpose((0, 4, 1, 2, 3))
                 # jax.debug.print("edge_bias_global: {edge_bias_global.shape}; position_bias_global: {position_bias_global.shape}", edge_bias_global=edge_bias_global, position_bias_global=position_bias_global)
                 # edge_bias_global = self.graph_edge_bias(edge_bias_global[:, :1].swapaxes(1, -1)[..., 0]).swapaxes(1, -1)
                 if not precomputed:
                     edge_bias_global = edge_bias_global[:, 0]
-                edge_bias_global = jnp.where(edge_bias_global[..., None]>=0, self.graph_edge_bias(edge_bias_global), jnp.zeros(tuple(edge_bias_global.shape) + (1,), dtype=self.dtype))
+                # edge_bias_global = jnp.where(edge_bias_global[..., None]>=0, self.graph_edge_bias(edge_bias_global), jnp.zeros(tuple(edge_bias_global.shape) + (1,)).astype(self.dtype))
+                edge_bias_global = jax.lax.select(edge_bias_global[..., None]>=0, self.graph_edge_bias(edge_bias_global), jnp.zeros(tuple(edge_bias_global.shape) + (1,)).astype(self.dtype))
                 position_bias_global = position_bias_global + edge_bias_global.transpose((0, 3, 1, 2))
 
             # if self.has_graph_edge_bias:
