02ba2fb78fe4a0a0b178cb19d3f97834737e71ba
gigant <giganttheo@gmail.com>
2024-02-04 12:58:01 +0100

    test with no jit

--- build_attn_graphs.py
+++ build_attn_graphs.py
@@ -7,12 +7,22 @@ import concurrent.futures
 import time
 import requests as r
 
+from datasets import Dataset
+
 from aga_transformers.attention_patterns.sparse_attention.global_dependency import prepare_global_dependency_attn_patterns
 
 def main():
 
     dataset = load_dataset('gigant/tib')
     tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+
+    with open("./dependency_graphs_tib.pickle", "rb") as file:
+        state_ = pickle.load(file)
+
+    new_dataset = {split: Dataset.from_list([{"id": k, **v} for k,v in state_[split].items() ]) for split in ["train", "valid", "test"]}
+
+    dataset_graphs=new_dataset
+
     graphs = {"train": {}, "valid": {}, "test": {}}
 
     for split in ["train", "valid", "test"]:
@@ -33,12 +43,13 @@ def main():
             print(f"{i} processed, / TOTAL={len(graphs[split])}")
         # for input in tqdm(enumerate(dataset[split])):
         #     get_graph(input)
-        inputs = list(enumerate(dataset[split]))
+        inputs = [(i, v) for i, v in list(enumerate(dataset[split])) if i not in dataset_graphs[split]["id"]]
+        # inputs = list(enumerate(dataset[split]))
         print(len(inputs))
         with concurrent.futures.ThreadPoolExecutor() as executor:
             executor.map(get_graph, inputs)
 
-    with open("dependency_graphs_tib.pickle", "wb") as outfile:
+    with open("dependency_graphs_tib_add.pickle", "wb") as outfile:
         pickle.dump(graphs, outfile, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
