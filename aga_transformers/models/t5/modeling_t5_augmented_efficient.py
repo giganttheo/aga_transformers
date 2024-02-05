614c1d0e05c61817bbb00a2d34d62aadfaa30528
gigant <giganttheo@gmail.com>
2024-02-05 22:18:41 +0100

    params for localglobal training

--- aga_transformers/models/t5/modeling_t5_augmented_efficient.py
+++ aga_transformers/models/t5/modeling_t5_augmented_efficient.py
@@ -1524,7 +1524,8 @@ class FlaxT5BlockCollection(nn.Module):
         if self.gradient_checkpointing:
             layer_outputs, other_outputs = scan_with_axes(FlaxT5LayerCollection, #remat(FlaxT5LayerCollection, static_argnums=(6, 7, 8)),
                             in_axes=(nn.broadcast, 1, nn.broadcast, nn.broadcast, nn.broadcast, nn.broadcast, nn.broadcast, nn.broadcast),
-                            split_rngs={'params': False},
+                            variable_axes=["params", "graphs"] ,
+                            split_rngs={'params': True},
                             # variable_broadcast=["graphs"],
                             axis_name='',
                             length=self.config.num_layers)(name="block", config=self.config, has_relative_attention_bias=True, dtype=self.dtype,)(
