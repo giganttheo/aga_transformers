a83aeca13f809197122b550c9f5e3077ad96cef5
gigant <giganttheo@gmail.com>
2024-02-08 19:38:13 +0100

    max batch size tests

--- train_lora_structural.py
+++ train_lora_structural.py
@@ -390,14 +390,13 @@ def data_loader(rng: jax.random.PRNGKey, dataset: Dataset, model, batch_size: in
     for idx in batch_idx:
         batch = dataset[idx]
         #broadcast to all layers
-        graph_batch = unroll_graph_to_scan(batch.pop("graph"), model.config.num_layers)
         # print([type(graph_batch[0][k]) for k in graph_batch[0].keys()])
         graph_batch = {
-            "receivers": np.stack([graph["receivers"] for graph in graph_batch]).astype(np.int16),
-            "senders": np.stack([graph["senders"] for graph in graph_batch]).astype(np.int16),
-            "graph_mask": np.stack([graph["graph_mask"] for graph in graph_batch]).astype("bool"),
-            "edge_labels": np.stack([graph["edge_labels"] for graph in graph_batch]).astype(np.int8),
-            "n_slides": np.stack([graph["n_slides"] for graph in graph_batch]).astype(np.int16),
+            "receivers": np.stack([np.stack([graph["receivers"] for graph in graph_batch])]*model.config.num_layers).astype(np.int16),
+            "senders": np.stack([np.stack([graph["senders"] for graph in graph_batch])]*model.config.num_layers).astype(np.int16),
+            "graph_mask": np.stack([np.stack([graph["graph_mask"] for graph in graph_batch])]*model.config.num_layers).astype("bool"),
+            "edge_labels": np.stack([np.stack([graph["edge_labels"] for graph in graph_batch])]*model.config.num_layers).astype(np.int8),
+            "n_slides": np.stack([np.stack([graph["n_slides"] for graph in graph_batch])]*model.config.num_layers).astype(np.int16),
             # "mask_local": np.stack([graph["mask_local"] for graph in graph_batch], dtype="bool"),
             # "mask_global": np.stack([graph["mask_global"] for graph in graph_batch], dtype="bool"),
             # "edge_bias_local": np.stack([graph["edge_bias_local"] for graph in graph_batch], dtype=np.int8),
