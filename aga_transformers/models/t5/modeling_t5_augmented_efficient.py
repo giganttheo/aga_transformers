911e0dd5b95164d50505d1a7185a9ef3157ff703
gigant <giganttheo@gmail.com>
2024-02-09 13:13:39 +0100

    max batch size tests

--- aga_transformers/models/t5/modeling_t5_augmented_efficient.py
+++ aga_transformers/models/t5/modeling_t5_augmented_efficient.py
@@ -1571,7 +1571,7 @@ class FlaxT5BlockCollection(nn.Module):
             edge_bias_global=None
 
         if self.gradient_checkpointing:
-            layer_outputs, _ = nn.scan(ScannableFlaxT5LayerCollection, #remat(ScannableFlaxT5LayerCollection, static_argnums=(4, 5, 6)), #remat(FlaxT5LayerCollection, static_argnums=(6, 7, 8)),
+            layer_outputs, _ = nn.scan(remat(ScannableFlaxT5LayerCollection, static_argnums=(4, 5, 6)), #remat(FlaxT5LayerCollection, static_argnums=(6, 7, 8)),
                             in_axes=(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
                             variable_axes={"params": 0}, #, "graphs": 0}, #==> instead of using the variables, we passe the input in the model
                             split_rngs={"params": True, "dropout": True},
