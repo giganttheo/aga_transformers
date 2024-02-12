894fe38598e47cce9558aa94006fb6459fa8fead
gigant <giganttheo@gmail.com>
2024-02-13 00:26:35 +0100

    prepare training

--- train_lora_structural.py
+++ train_lora_structural.py
@@ -816,6 +816,7 @@ def main():
         eval_dataset = eval_dataset.map(
             preprocess_function,
             batched=True,
+            batch_size=100,
             num_proc=data_args.preprocessing_num_workers,
             remove_columns=column_names,
             load_from_cache_file=not data_args.overwrite_cache,
@@ -832,6 +833,7 @@ def main():
         predict_dataset = predict_dataset.map(
             preprocess_function,
             batched=True,
+            batch_size=100,
             num_proc=data_args.preprocessing_num_workers,
             remove_columns=column_names,
             load_from_cache_file=not data_args.overwrite_cache,
