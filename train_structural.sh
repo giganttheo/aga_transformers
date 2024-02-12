a31b3699285b842f347d16949c4a1e08b85a8e75
gigant <giganttheo@gmail.com>
2024-02-12 10:28:09 +0100

    prepare training

--- train_structural.sh
+++ train_structural.sh
@@ -33,8 +33,8 @@ python ./train_lora_structural.py \
 	--max_source_length 8192 \
 	--val_max_target_length 512 \
 	--gradient_checkpointing \
-	# --max_train_samples 64 \
-	# --max_eval_samples 64 \
+	--max_train_samples 64 \
+	--max_eval_samples 64 \
 	# --resume_from_checkpoint \#8192 \
 	# --run_id "fv3mirpt"
 	# --max_train_samples 64 \
