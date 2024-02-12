a2b2271602ccdd1af9b588d660e67e35e2cb4038
gigant <giganttheo@gmail.com>
2024-02-12 10:19:59 +0100

    prepare training

--- train_structural.sh
+++ train_structural.sh
@@ -22,19 +22,19 @@ python ./train_lora_structural.py \
 	--source_prefix "summarize: " \
 	--do_train \
 	--do_eval \
-	--num_train_epochs 10 \
+	--num_train_epochs 5 \
 	--learning_rate 1e-2 \
 	--warmup_steps 100 \
-	--per_device_train_batch_size 10 \
-	--per_device_eval_batch_size 10 \
+	--per_device_train_batch_size 14 \
+	--per_device_eval_batch_size 14 \
 	--overwrite_output_dir \
 	--dtype "bfloat16" \
 	--max_target_length 512 \
 	--max_source_length 8192 \
 	--val_max_target_length 512 \
 	--gradient_checkpointing \
-	--max_train_samples 64 \
-	--max_eval_samples 64 \
+	# --max_train_samples 64 \
+	# --max_eval_samples 64 \
 	# --resume_from_checkpoint \#8192 \
 	# --run_id "fv3mirpt"
 	# --max_train_samples 64 \
