1ad2882fed3c49260c0f07b3690a6315a0d5e2af
gigant <giganttheo@gmail.com>
2024-02-13 13:49:06 +0100

    prepare training

--- generate_examples.py
+++ generate_examples.py
@@ -10,7 +10,7 @@ from functools import partial
 
 import jax
 
-test_dataset = load_dataset("gigant/tib", split="test").select(range(3))
+test_dataset = load_dataset("gigant/tib", split="test").select(range(100))
 
 generation_config = {
     "num_beams": 4, #instead of 2?
