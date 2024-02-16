bd0d73bf38b37af02af68bdc476b6e5c99adeb6e
gigant <giganttheo@gmail.com>
2024-02-16 20:17:08 +0100

    prepare training

--- aga_transformers/models/t5/modeling_t5_efficient.py
+++ aga_transformers/models/t5/modeling_t5_efficient.py
@@ -921,7 +921,7 @@ class FlaxT5EfficientBlockGraphSelfAttention(nn.Module):
         no_graph=False
         if mask_local is not None:
             print(f"shapes in attn: local {mask_local.shape}, global: {mask_global.shape}")
-            jax.debug.print("shapes in attn: local {mask_local.shape}, global: {mask_global.shape}", mask_local=mask_local, mask_global)
+            jax.debug.print("shapes in attn: local {mask_local.shape}, global: {mask_global.shape}", mask_local=mask_local, mask_global=mask_global)
             precomputed = True
         elif self.has_variable("graph", "mask_local"):
             mask_local = self.variables["graph"]["mask_local"].astype("bool")
