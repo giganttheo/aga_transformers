5afe32e3b6eb6373700f8d0474d237401e1a5f9f
gigant <giganttheo@gmail.com>
2024-02-19 11:58:19 +0100

    prepare training

--- aga_transformers/models/t5/modeling_t5_efficient.py
+++ aga_transformers/models/t5/modeling_t5_efficient.py
@@ -1094,7 +1094,7 @@ class FlaxT5LayerSelfAttention(nn.Module):
     dtype: jnp.dtype = jnp.float32  # the dtype of the computation
 
     def setup(self):
-        if self.config.causal:
+        if self.config.causal or True:
             self.SelfAttention = FlaxT5Attention(
                 self.config,
                 has_relative_attention_bias=self.has_relative_attention_bias,
@@ -1404,7 +1404,7 @@ class FlaxT5BlockCollection(nn.Module):
             if self.config.causal and encoder_hidden_states is not None:
                 carry_ += (encoder_decoder_position_bias, )
 
-            layer_outputs, _ = nn.scan(ScannableFlaxT5LayerCollection, #remat(ScannableFlaxT5LayerCollection, static_argnums=(4, 5, 6)), #remat(FlaxT5LayerCollection, static_argnums=(6, 7, 8)),
+            layer_outputs, _ = nn.scan(remat(ScannableFlaxT5LayerCollection, static_argnums=(4, 5, 6)), #remat(FlaxT5LayerCollection, static_argnums=(6, 7, 8)),
                             in_axes=(nn.broadcast, nn.broadcast, nn.broadcast, nn.broadcast, nn.broadcast, nn.broadcast), # 0, 0, 0, 0, 0, 0, 0),
                             variable_axes={"params": 0, "graphs": 0}, #==> instead of using the variables, we passe the input in the model
                             split_rngs={"params": True, "dropout": True},
