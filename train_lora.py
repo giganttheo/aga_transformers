94812c5b4ceb52660ddc8dd5a8a77cf1bddbb9f4
gigant <giganttheo@gmail.com>
2024-02-20 01:08:07 +0100

    prepare training

--- train_lora.py
+++ train_lora.py
@@ -578,7 +578,8 @@ def main():
         print("=============================")
         print("Enabling gradient checkpointing")
         print("=============================")
-        model.enable_gradient_checkpointing()
+        # model.enable_gradient_checkpointing()
+        model.enable_scan()
 
     if model.config.decoder_start_token_id is None:
         raise ValueError("Make sure that `config.decoder_start_token_id` is correctly defined")
