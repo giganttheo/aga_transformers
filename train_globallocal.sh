bf2613bbbd21a2075ad21b526d25263470c0a125
gigant <giganttheo@gmail.com>
2024-02-21 01:37:54 +0100

    prepare training

--- train_globallocal.sh
+++ train_globallocal.sh
@@ -248,16 +248,16 @@ python ./train_lora.py \
 	--num_train_epochs 10 \
 	--learning_rate 1e-2 \
 	--warmup_steps 100 \
-	--per_device_train_batch_size 12 \
-	--per_device_eval_batch_size 12 \
+	--per_device_train_batch_size 14 \
+	--per_device_eval_batch_size 14 \
 	--overwrite_output_dir \
 	--dtype "bfloat16" \
 	--max_target_length 512 \
 	--max_source_length 8192 \
 	--val_max_target_length 512 \
 	--gradient_checkpointing \
-	--max_train_samples 50 \
-	--max_eval_samples 50 \
+	# --max_train_samples 50 \
+	# --max_eval_samples 50 \
 	# --seed 43 \
 	# --resume_from_checkpoint \
 	# --run_id "fv3mirpt"
