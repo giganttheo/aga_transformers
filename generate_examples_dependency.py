e24c6542ff1d089e5cb775d7a2097183aee77c4c
gigant <giganttheo@gmail.com>
2024-02-28 20:06:22 +0100

    prepare training

--- generate_examples_dependency.py
+++ generate_examples_dependency.py
@@ -34,7 +34,7 @@ generation_config = {
 #     early_stopping = True,
 #     no_repeat_ngram_size = 3)
 
-repo_path= "gigant/graphlongt5-globallocal-0228" #"gigant/longt5-global-3epoch" #"gigant/graph-t5-global-window-8k-longt5local" # ==> my checkpoint
+repo_path= "gigant/graphlongt5-dependency-0228" #"gigant/longt5-global-3epoch" #"gigant/graph-t5-global-window-8k-longt5local" # ==> my checkpoint
 attention_kwargs={
             "max_source_length": 8192,
             "max_target_length": 512,
---
+++ generate_examples_dependency.sh
@@ -0,0 +1,12 @@
+conda activate train-jax
+
+export http_proxy=http://webproxy.lab-ia.fr:8080
+export https_proxy=http://webproxy.lab-ia.fr:8080
+export HTTP_PROXY=http://webproxy.lab-ia.fr:8080
+export HTTPS_PROXY=http://webproxy.lab-ia.fr:8080
+cd ~/graph-transformer/aga_transformers
+export PATH=/usr/local/cuda-11.2/bin:$PATH.
+export PATH=/usr/local/cuda-10.2/targets/x86_64-linux/include:$PATH.
+export TOKENIZERS_PARALLELISM=false
+
+python ./generate_examples_dependency.py
