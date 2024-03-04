7b95f8dfb93f645598211f964bd176a345fdc11c
gigant <giganttheo@gmail.com>
2024-03-04 17:25:20 +0100

    prepare training

--- generate_examples_longt5.py
+++ generate_examples_longt5.py
@@ -12,13 +12,13 @@ from functools import partial
 
 import jax
 
-test_dataset = load_dataset("gigant/tib", split="test").select(range(10))
+test_dataset = load_dataset("gigant/tib", split="test").select(range(30))
 
 generation_config = {
     "num_beams": 3, #instead of 2?
     "max_new_tokens": 512,
     # "min_length": 1,
-    "length_penalty": 3,
+    "length_penalty": -2,
     "early_stopping": True,
     # "no_repeat_ngram_size": 3,
 }
