8f2753a05c1c1dc243c0262223138769862e8949
gigant <giganttheo@gmail.com>
2024-03-07 13:50:23 +0100

    prepare training

--- generate_examples.py
+++ generate_examples.py
@@ -13,7 +13,7 @@ import jax
 test_dataset = load_dataset("gigant/tib", split="test").select(range(10))
 
 generation_config = {
-    "num_beams": 1, #instead of 2?
+    "num_beams": 3, #instead of 2?
     "max_new_tokens": 512,
     # "min_length": 1,
     "length_penalty": -2.,
