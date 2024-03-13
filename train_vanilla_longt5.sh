8fb8dd587533cd5b8c7f48872eb4f933354d2121
gigant <giganttheo@gmail.com>
2024-03-13 11:23:53 +0100

    change settings

--- train_vanilla_longt5.sh
+++ train_vanilla_longt5.sh
@@ -20,11 +20,11 @@ python ./train_longt5_vanilla.py \
 	--source_prefix "summarize: " \
 	--do_train \
 	--do_eval \
-	--num_train_epochs 6 \
+	--num_train_epochs 10 \
 	--learning_rate 1e-2 \
 	--warmup_steps 100 \
-	--per_device_train_batch_size 14 \
-	--per_device_eval_batch_size 14 \
+	--per_device_train_batch_size 16 \
+	--per_device_eval_batch_size 16 \
 	--overwrite_output_dir \
 	--dtype "bfloat16" \
 	--max_target_length 512 \
