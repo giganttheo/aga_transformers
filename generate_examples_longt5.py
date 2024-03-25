495e3bb9a3aaa37052cf84fa96a890d5fe91bee2
gigant <giganttheo@gmail.com>
2024-03-25 14:41:21 +0100

    structural and dependency bias model

--- generate_examples_longt5.py
+++ generate_examples_longt5.py
@@ -8,11 +8,15 @@ import transformers
 from transformers import AutoTokenizer, FlaxLongT5ForConditionalGeneration
 from datasets import load_dataset
 
+import numpy as np
+
+import math
+
 from functools import partial
 
 import jax
 
-test_dataset = load_dataset("gigant/tib", split="test").select(range(50))
+test_dataset = load_dataset("gigant/tib", split="test")
 
 generation_config = {
     "num_beams": 2,
@@ -24,7 +28,7 @@ generation_config = {
 
 # generation_config = transformers.GenerationConfig(**generation_config)
 
-repo_path= "gigant/longt5-0229" #"gigant/longt5-global-3epoch" #"gigant/graph-t5-global-window-8k-longt5local" # ==> my checkpoint
+repo_path= "gigant/longt5-0322" #"gigant/longt5-global-3epoch" #"gigant/graph-t5-global-window-8k-longt5local" # ==> my checkpoint
 
 tokenizer = AutoTokenizer.from_pretrained(repo_path)
 model = FlaxLongT5ForConditionalGeneration.from_pretrained(repo_path, dtype="bfloat16")
@@ -36,36 +40,79 @@ decoder_start_token_id = model.config.decoder_start_token_id
 
 batch_size = 12
 
-# @partial(jax.jit)
-# def generate(input_ids, attention_mask, params):
-#     pred_ids = model.generate(input_ids, generation_config=generation_config, attention_mask=attention_mask, decoder_start_token_id=decoder_start_token_id, params=params)
-#     return tokenizer.batch_decode(pred_ids.sequences, skip_special_tokens=True)
+prefix = "summarize: "
+max_source_length=8192
+
+def preprocess_function(examples):
+    inputs = examples["transcript"]
+    label = examples["abstract"]
+    inputs = [prefix + inp for inp in inputs]
+    model_inputs = tokenizer(
+        inputs, max_length=max_source_length, padding="max_length", truncation=True, return_tensors="np"
+    )
+    model_inputs["label"] = label
+    return model_inputs
+
+test_dataset = test_dataset.map(
+    preprocess_function,
+    batched=True,
+    batch_size=500,
+    num_proc=1,
+    remove_columns=test_dataset.column_names,
+    desc="Running tokenizer on test dataset",
+)
+
+def data_loader(rng, dataset, batch_size, shuffle: bool = False, drop_last=True):
+    """
+    Returns batches of size `batch_size` from `dataset`. If `drop_last` is set to `False`, the final batch may be incomplete,
+    and range in size from 1 to `batch_size`. Shuffle batches if `shuffle` is `True`.
+    """
+    if shuffle:
+        batch_idx = jax.random.permutation(rng, len(dataset))
+        batch_idx = np.asarray(batch_idx)
+    else:
+        batch_idx = np.arange(len(dataset))
+
+    if drop_last:
+        steps_per_epoch = len(dataset) // batch_size
+        batch_idx = batch_idx[: steps_per_epoch * batch_size]  # Skip incomplete batch.
+        batch_idx = batch_idx.reshape((steps_per_epoch, batch_size))
+    else:
+        steps_per_epoch = math.ceil(len(dataset) / batch_size)
+        batch_idx = np.array_split(batch_idx, steps_per_epoch)
+
+    for idx in batch_idx:
+        batch = dataset[idx]
+        label = batch.pop("label")
+        batch = {k: np.array(v) for k, v in batch.items()}
+        yield batch, label
+
+test_loader = data_loader(jax.random.PRNGKey(0), test_dataset, batch_size, shuffle = True, drop_last=True)
+
+
+predictions = []
+references = []
+params=model.params
+decoder_start_token_id = model.config.decoder_start_token_id
 
 # @jax.jit
-def generate(input_ids, inputs, params):
+def generate(input_ids, inputs):
     pred_ids = beam_search(model, params, input_ids, inputs, length_penalty=generation_config["length_penalty"], batch_size=batch_size, num_beams=generation_config["num_beams"], no_repeat_ngram_size=generation_config["no_repeat_ngram_size"])
     return tokenizer.batch_decode(pred_ids.sequences, skip_special_tokens=True)
 
-for rec in tqdm(test_dataset):
-    text = "summarize: " + rec["transcript"]
-    label = rec["abstract"]
-    inputs = tokenizer(text, return_tensors="np", truncation=True, max_length=8192)
-    # label_ids = tokenizer(label, return_tensors="pt").input_ids
-    input_ids = inputs.pop("input_ids")
-    # attention_mask = inputs.pop("attention_mask")
-    preds = generate(input_ids, inputs, model.params)
-    # pred_ids = generate(inputs["input_ids"], inputs["attention_mask"], params)
-    predictions.append(preds)
-    references.append(label)
+for batch, label in tqdm(test_loader):
+    input_ids = batch.pop("input_ids")
+    preds = generate(input_ids, batch)
+    predictions.extend(preds)
+    references.extend(label)
 
 # open file in write mode
-with open('predictions.txt', 'w') as fp:
+with open('predictions_long.txt', 'w') as fp:
     for line in predictions:
         # write each item on a new line
-        fp.write(line[0] + "\n")
+        fp.write(line + "\n")
 # open file in write mode
-with open('references.txt', 'w') as fp:
+with open('references_long.txt', 'w') as fp:
     for line in references:
         # write each item on a new line
         fp.write(line + "\n")
-
