9a5c33140f443230acaeea5f1fbea55aaaa2a4c6
gigant <giganttheo@gmail.com>
2024-04-10 00:18:14 +0200

    prepare trainig

--- aga_transformers/models/t5/modeling_t5_slides.py
+++ aga_transformers/models/t5/modeling_t5_slides.py
@@ -1218,7 +1218,7 @@ class FlaxT5EfficientBlockGraphSelfAttention(nn.Module):
         # # compute position bias
 
         position_bias_local = self._create_block_position_bias(block_len, n_global_tokens_context, num_blocks, n_document_tokens, n_slides_total)
-        position_bias_global = self.compute_bias(query_length=n_global_tokens, key_length=seq_length)[None]
+        position_bias_global = einops.repeat(self.compute_bias(query_length=n_global_tokens, key_length=seq_length), "h n d -> b h n d", b=batch_size)
         
         if self.has_graph_edge_bias and not no_graph:
             # jax.debug.print("edge_bias_local: {edge_bias_local.shape}; position_bias_local: {position_bias_local.shape}", edge_bias_local=edge_bias_local, position_bias_local=position_bias_local)
