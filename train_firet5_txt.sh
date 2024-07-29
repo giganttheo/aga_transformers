a0535172563c2d7b970207a26aa63660a6f950fb
gigant <giganttheo@gmail.com>
2024-07-29 07:01:58 +0200

    activate tensorboard

--- train_firet5_txt.sh
+++ train_firet5_txt.sh
@@ -21,14 +21,14 @@ python ./train_FIRE_txt.py \
 	--do_train \
 	--do_eval \
 	--num_train_epochs 10 \
-	--learning_rate 2e-2 \
+	--learning_rate 4e-2 \
 	--warmup_steps 100 \
-	--per_device_train_batch_size 32 \
-	--per_device_eval_batch_size 32 \
+	--per_device_train_batch_size 64 \
+	--per_device_eval_batch_size 64 \
 	--overwrite_output_dir \
 	--dtype "bfloat16" \
 	--max_target_length 512 \
-	--max_source_length 8192 \
+	--max_source_length 4096 \
 	--val_max_target_length 512 \
 	--gradient_checkpointing \
     # --wandb_api_key $WANDB_API_KEY \
