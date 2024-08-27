9ef6d75be1664345e72256f10b87fa12ad002be2
gigant <giganttheo@gmail.com>
2024-08-27 15:54:41 +0200

    attempting firet5 with qa dataset

--- train_FIRE_vqa-text.py
+++ train_FIRE_vqa-text.py
@@ -773,11 +773,10 @@ def main():
                 #token distance
                 tok_dist = j - i
 
-                #horizontal and vertical distances
-                box_id1 = box_ids_[i]
-                box_id2 = box_ids_[j]
-
-                if box_id1 is not None and box_id2 is not None:
+                if len(box_ids_) >= max(i,j) and box_ids_[i] is not None and box_ids_[j] is not None:
+                    #horizontal and vertical distances
+                    box_id1 = box_ids_[i]
+                    box_id2 = box_ids_[j]
                     hd = hdist[box_id1, box_id2]
                     vd = vdist[box_id1, box_id2]
                 else:
